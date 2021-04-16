84e1c76f26c964a4afcbcdcaa7f91ca6c27ccd9b
Landy Simpson <simpslandyy@gmail.com>
2021-04-15 22:46:43 -0400

    Fix up CSS rendering

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,34 @@
+import React from 'react'
 import { AppProps } from 'next/app'
+import Head from 'next/head';
+import { StylesProvider, ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Roster App</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <StylesProvider injectFirst>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </StylesProvider>
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default App
