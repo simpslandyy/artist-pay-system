ad3ff41025ad06224b74fac89b79a9fc4a96e0a3
Landy Simpson <simpslandyy@gmail.com>
2023-01-11 22:38:32 -0500

    fix migration script

--- server/migration/1673493533343-AddArtistsToRoster.ts
+++ server/migration/1673493533343-AddArtistsToRoster.ts
@@ -2,16 +2,17 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 import MOCK from '../../roster'
 const { data } = MOCK
 
-export class AddArtists1673490552040 implements MigrationInterface {
+export class AddArtistsToRoster1673493533343 implements MigrationInterface {
+    name = 'AddArtistsToRoster1673493533343'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         for(let d of data) {
-            await queryRunner.query(`INSERT INTO 'roster' (artist, rate, streams) VALUES (${d.artist}, ${d.rate}, ${d.streams}});`)
+            await queryRunner.query(`INSERT INTO roster (artist, rate, streams) VALUES ('${d.artist}', ${d.rate}, ${d.streams});`)
         }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`TRUNCATE 'roster;`)
+        await queryRunner.query(`TRUNCATE roster;`)
     }
 
 }
